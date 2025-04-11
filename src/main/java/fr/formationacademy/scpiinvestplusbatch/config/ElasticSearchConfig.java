9a2a5489c9ea36289756c1f00c71da23606dc0cb
AitAhmedNasser <aitahmednasser@outlook.com>
2025-04-11 13:59:20 +0100

    wip

--- src/main/java/fr/formationacademy/scpiinvestplusbatch/config/ElasticSearchConfig.java
+++ src/main/java/fr/formationacademy/scpiinvestplusbatch/config/ElasticSearchConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.http.HttpHost;
 
-
 @Configuration
 public class ElasticSearchConfig {
     @Value("${spring.elasticsearch.hostname}")
